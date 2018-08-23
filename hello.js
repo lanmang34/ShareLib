169e770d7888511f35b6239bacf0d0457e667eb5
lanmang34 <623861008@qq.com>
2018-08-23 14:15:54 +0800

    Create hello.js
    weex test js

---
+++ hello.js
@@ -0,0 +1,13 @@
+<template>
+  <div style="justify-content:center">
+    <text class="freestyle">HELLO WORLD</text>
+  </div>
+</template>
+
+<style scoped>
+  .freestyle {
+    color: #41B883;
+    font-size: 233px;
+    text-align: center;
+  }
+</style>
